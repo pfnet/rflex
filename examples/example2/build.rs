4d1429074071c030715fa970220ce38b5a8cba4f
Shuzo Kashihara <kashihara@preferred.jp>
2019-04-11 11:52:55 +0900

    Suppress warnings and add error handling in build.rs

--- examples/example2/build.rs
+++ examples/example2/build.rs
@@ -4,5 +4,8 @@ use std::path::Path;
 fn main() {
     let path = Path::new("src").join("test.l");
     let path = path.to_str().unwrap().to_string();
-    rflex::process(path);
+    if let Err(e) = rflex::process(path) {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
 }
